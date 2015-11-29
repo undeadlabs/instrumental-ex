80ec4c12512a76e51fa82243c4a84e2791396b72
Joel "The Merciless" Meador <joel@expectedbehavior.com>
2015-11-28 21:14:50 -0500

    Update time.ex
    Fix docs for `Instrumental.Time.unix_monotonic` and make it simpler by using `system_time`.

--- lib/instrumental/time.ex
+++ lib/instrumental/time.ex
@@ -6,11 +6,10 @@
 
 defmodule Instrumental.Time do
   @doc """
-  Unix timestamp in milliseconds since epoch 1970-01-01.
+  Unix timestamp in seconds since epoch 1970-01-01.
   """
   @spec unix_monotonic :: integer
   def unix_monotonic do
-    {mega, sec, _} = :erlang.now
-    mega * 1_000_000 + sec
+    :erlang.system_time(:seconds)
   end
 end
